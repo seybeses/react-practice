56949da1175a7003659db9e39d14024d520830bb
seybeses <syb9822446@naver.com>
2025-11-09 21:26:30 +0900

    20251109

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
-  <h1>안녕 리액트!</h1>
-)
+<div id="root">
+  <h1 id="title">가위바위보 게임임</h1>
+  <h2>패를 골라주세요</h2>
+  <button className="hand">가위</button>
+  <button className="hand">바위</button>
+  <button className="hand">보</button>
+</div>
+
+);
